783420806dda8c18c80a6ae9292cc8f63026cc04
abibimaryam <rriarij@gmail.com>
2025-05-19 16:18:59 +0500

    Update agents.py

--- agents.py
+++ agents.py
@@ -20,7 +20,7 @@ def search_tool(query: str) -> str:
             results = [r["body"] for r in ddgs.text(query, max_results=3)]
         return "\n\n".join(results)
 
-# Создание агента 
+# Создание агента интервьюера
 interviewer = AgentWorkflow.from_tools_or_functions(
     tools_or_functions=[search_tool],
     llm=llm,
@@ -56,10 +56,53 @@ async def conversation():
             history.append({"role": "assistant", "content": response})
         except Exception as e:
             print(f"[Ошибка] {e}")
-     
+
+
+
+
+async def analyze(conv):
+    # Создание агента анализатора
+    analyzer = AgentWorkflow.from_tools_or_functions(
+        tools_or_functions=[search_tool],
+        llm=llm,
+        system_prompt = (""), # сюда надо написать системный промпт
+    ) 
+
+    #здесь надо Запустить агента написать ему промпт,где передается история и получить выжимку
+    #пока пусть будет 
+    summarize_conv=""
+    return summarize_conv
+
+
+async def pass_a_verdict(summarize_conv):
+    verdict=""
+    return verdict
+
+
+async def risk_estimate(summarize_conv,verdict):
+    # Создание агента риск оценщика
+    estimater = AgentWorkflow.from_tools_or_functions(
+        tools_or_functions=[search_tool],
+        llm=llm,
+        system_prompt = (""), # сюда надо написать системный промпт
+    ) 
+    #здесь надо Запустить агента написать ему промпт,где передается краткая выжимка и вердикт. И получить риски
+    #пока пусть будет 
+    risks=""
+    return risks
+
+
+
 async def main():
-    conv = await conversation()
+    conv = await conversation() # В conv хранится история агента интервьюера с юзером 
     # print(conv)
+    summarize_conv=analyze(conv=conv)
+    verdict=pass_a_verdict(summarize_conv=summarize_conv)
+    risks=risk_estimate(summarize_conv=summarize_conv,verdict=verdict)
+
+
+
+
 
 if __name__ == "__main__":
     asyncio.run(main())
