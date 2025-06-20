d1bed0a3bf8414ee89d80fabcabfbed961e50b69
abibimaryam <rriarij@gmail.com>
2025-06-20 16:35:26 +0500

    removed pycache, added sample env,added requirements

--- telegram_bot/keyboards.py
+++ telegram_bot/keyboards.py
@@ -3,9 +3,9 @@ from telegram import ReplyKeyboardMarkup, KeyboardButton
 def get_main_keyboard() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="/register"), KeyboardButton(text="/login"), KeyboardButton(text="/logout")],
-            [KeyboardButton(text="/ask_AI"), KeyboardButton(text="/restart_session")],
-            [KeyboardButton(text="/ask_lawyer"), KeyboardButton(text="/ask_lawyer")],
+            [KeyboardButton(text="/register"), KeyboardButton(text="/login")],
+            [KeyboardButton(text="/logout"),KeyboardButton(text="/restart_session")],
+            [KeyboardButton(text="/ask_AI"),  KeyboardButton(text="/ask_lawyer")],
             [KeyboardButton(text="/help"), KeyboardButton(text="/cancel")],
             [KeyboardButton(text="/start")]
         ],
