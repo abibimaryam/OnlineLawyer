1db2378c66ee31a2a9dc47c6741f9dc1e6fda095
Muza005 <azimovmuzaffar90@gmail.com>
2025-06-25 20:21:47 +0500

    Create test_registration.py by Kamola

---
+++ test_registration.py
@@ -0,0 +1,18 @@
+# test_registration.py
+from telegram_bot.bot import get_second_name
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from unittest.mock import AsyncMock
+
+import pytest
+
+@pytest.mark.asyncio
+async def test_user_registration_step():
+    state = AsyncMock()
+    message = AsyncMock(spec=Message)
+    message.text = "Иванов"
+    await get_second_name(message, state)
+
+    state.update_data.assert_called_once_with(second_name="Иванов")
+    state.set_state.assert_called_once()
+    message.answer.assert_called_once()
