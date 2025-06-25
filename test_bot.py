9ed9bee06ec8f2e7c45be6c52ec4cd6268cddcb2
Muza005 <azimovmuzaffar90@gmail.com>
2025-06-25 20:21:23 +0500

    Create test_bot.py by Kamola

---
+++ test_bot.py
@@ -0,0 +1,18 @@
+import pytest
+from telegram_bot.bot import set_personal_commands
+from unittest.mock import AsyncMock, patch
+
+@pytest.mark.asyncio
+@patch("aiosqlite.connect")
+async def test_set_personal_commands_for_user(mock_connect):
+    user_id = 1111
+    mock_conn = AsyncMock()
+    mock_cursor = AsyncMock()
+    mock_cursor.fetchone.side_effect = [(1,), None]  # юзер есть, юрист — нет
+    mock_conn.execute.return_value = mock_cursor
+    mock_connect.return_value.__aenter__.return_value = mock_conn
+
+    bot = AsyncMock()
+    await set_personal_commands(bot, user_id)
+
+    bot.set_my_commands.assert_called()
